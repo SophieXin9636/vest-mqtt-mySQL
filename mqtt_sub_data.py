a80e1d4d7c26a4ebd48373225b85b248df27bcc7
Sophiexin9636 <sophiexin036@gmail.com>
2020-09-24 21:20:48 +0800

    update mqtt connection

--- mqtt_sub_data.py
+++ mqtt_sub_data.py
@@ -3,39 +3,43 @@ import paho.mqtt.client as mqtt
 import save_data_to_DB
 
 # MQTT Settings 
-MQTT_broker = "140.117.189.242"
-MQTT_port = 1883
+MQTT_broker = ""
 Keep_Alive_Interval = 45
 MQTT_topic = "Home/Force"
 
 # Subscribe to all Sensors for the Topic
-def on_connect(mosq, obj, rc):
-	mqttc.subscribe(MQTT_topic, 0)
+def on_connect(mosq, data, flags, rc):
+	#print("Connected with result code: " + str(rc))
+	print("MQTT connect!")
+	client.subscribe(MQTT_topic)
 
 # Save Data into DB Table
-def on_message(mosq, obj, msg):
+def on_message(mosq, data, msg):
 	# This is the Master Call for saving MQTT Data into DB
 	# refer to "save_data_to_DB.py" "sensor_Data_Handler" function
-	print("MQTT Data Received...")
-	print("MQTT Topic: " + msg.topic)
-	print("Data: " + msg.payload)
+	print("MQTT Data Received... ")
+	print(msg.payload.decode())
+	#print("MQTT Topic: " + msg.topic)
+	#print("Data: " + msg.payload)
 	sensor_Data_Handler(msg.topic, msg.payload)
 
-def on_subscribe(mosq, obj, mid, granted_qos):
-    pass
+def on_subscribe(mosq, data, mid, granted_qos):
+	pass
 
-mqttc = mqtt.Client()
+client = mqtt.Client()
 
 # Assign event callbacks
-mqttc.on_message = on_message
-mqttc.on_connect = on_connect
-mqttc.on_subscribe = on_subscribe
+client.on_message = on_message
+client.on_connect = on_connect
+client.on_subscribe = on_subscribe
 
-#mqttc.username_pw_set(user, password)
+user = ""
+password = ""
+client.username_pw_set(user, password)
 
 # Connect
-mqttc.connect(MQTT_broker, int(MQTT_port), int(Keep_Alive_Interval))
-print("MQTT connect!")
+client.connect("192.168.43.30", 1883, int(Keep_Alive_Interval))
+#print("MQTT connect!")
 
 # Continue the network loop
-mqttc.loop_forever()
+client.loop_forever()
--- save_data_to_DB.py
+++ save_data_to_DB.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import json
-import pymqysql
+import pymysql
 
 # MySQL DB Name
 DB_Name =  "Pat_IoT.db"
